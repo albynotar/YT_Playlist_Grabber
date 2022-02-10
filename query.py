83e4f95342094c0e814a444ac9ca651600d82ff3
albynotar <77164768+albynotar@users.noreply.github.com>
2022-02-10 21:43:48 +0100

    query check process with regex

--- query.py
+++ query.py
@@ -1,51 +1,44 @@
-from urllib.parse import urlparse
 import yt_dlp
 import re
 
 """
 Function that checks and cleans a query inserted as input.
-The query must be a valid yt-playlist url to pass the validation.
+The query must contain a valid yt-playlist ID to pass the validation.
 """
 
 
 def check_query(query):
-    if query == '':
-        return False, 'Your Query is empty'
     # remove any special character from query
     url = re.sub(r'\s+', '', query)
 
     # adjusted query
-    clear_url = ''
+    playlist_id = re.findall(r'[0-9A-Za-z_-]{24,34}', url)
 
-    # checks for a youtube domain
-    if url.startswith('https://www.youtube.com/'):
-        clear_url = str(url)
-    elif url.startswith('//www.youtube.com/'):
-        clear_url = str('https:') + str(url)
-    elif url.startswith('www.youtube.com/'):
-        clear_url = str('https://') + str(url)
-    elif url.startswith('youtube.com/'):
-        clear_url = str('https://www.') + str(url)
-
-    # if the url does not contain a youtube url domain return False
-    if clear_url == '':
-        return False, 'Your Query is not a youtube link'
-
-    # analyze the query using urlparse
-    o = urlparse(clear_url)
-
-    # if the link does not contain a playlist query return False
-    if 'list=' not in o.query:
-        return False, 'Your Query is not a youtube playlist url'
-
-    # if the link does not contain a playlist id return False
-    if o.query.endswith('list='):
+    if not playlist_id:
         return False, 'Your Query does not contain a youtube playlist id'
+    else:
+        clear_url = 'https://www.youtube.com/playlist?list=' + str(playlist_id[0])
+
+    # settings for logger to not print anything in command line
+    class Logger:
+
+        def debug(self, msg):
+            pass
+
+        def info(self, msg):
+            pass
+
+        def warning(self, msg):
+            pass
+
+        def error(self, msg):
+            pass
 
     # options to extract info
     ydl_opts = {
+        'logger': Logger(),
         'forcejson': True,
-        'ignoreerrors': True,
+        'ignoreerrors': False,
         'skip_download': True,
         'playlistrandom': True,
         'playliststart': 1,
@@ -59,7 +52,11 @@ def check_query(query):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.cache.remove()
         # extract info
-        info = ydl.extract_info(clear_url, download=False)
+        try:
+            info = ydl.extract_info(clear_url, download=False)
+        except (yt_dlp.utils.DownloadError, yt_dlp.utils.ExtractorError):
+            return False, 'Your Query does not contain a valid youtube playlist id'
+
         if info is None:
             # if info not extracted (non valid url)
             return False, 'Your Query does not contain a valid youtube playlist id'
@@ -70,20 +67,7 @@ def check_query(query):
 
 if __name__ == '__main__':
     # DEBUG
-    print('WORK')
-    print(check_query('https://www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('ps://www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('//www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('https://www.youtube.com/watch?v=AEtbFm_CjE0&list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX'))
-    print(check_query('https://www.youtube.com/watch?v=AEtbFm_CjE0&list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX&index=1'))
-    print(check_query('https://www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uX&watch?v=AEtbFm_CjE0'))
-    print(check_query('https://www.youtube.com/playlist?list=PLNyOBdEynvBp1E\t03WBh33-P\rhBG9 WSQ-uX\n'))
-
-    print('\nERROR')
-    print(check_query(''))  # empty string
-    print(check_query('dfg\rhuyt|df-.\n\t'))  # random string
-    print(check_query('https://www.youtube.com/watch?v=jNQXAC9IVRw'))  # youtube video link
-    print(check_query('https://www.youtube.com/playlist?list='))  # empty playlist id
-    print(check_query('https://www.youtube.com/playlist?list=PLNyOBdEynvBp1E03WBh33-PhBG9WSQ-uC'))  # invalid playlist id
+    with open('query_tests.txt', 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            print(check_query(line))
