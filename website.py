37cf5c53bf02295ae4f79016fb6922649ab280b4
albynotar <77164768+albynotar@users.noreply.github.com>
2022-02-11 15:30:51 +0100

    added http error handling

--- website.py
+++ website.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file, redirect
 from query import check_query
 from extraction import processing
 from flask_caching import Cache
+from werkzeug.exceptions import HTTPException
 
 """
 Webpage to ask and receive particular information about a yt playlist.
@@ -51,7 +52,7 @@ def result():
             # reduce query length if too long > 50
             if len(playlist_url) > 50:
                 playlist_url = playlist_url[:20] + ' [...] ' + playlist_url[-20:]
-            return render_template('error.html', query=playlist_url, error_message=error_message)
+            return render_template('query_error.html', query=playlist_url, error_message=error_message)
         # if query check passed
         else:
             # extract info using extraction.py processing function
@@ -69,23 +70,37 @@ def result():
 
 
 # route for downloading a data table as json
-@app.route('/download', methods=['POST'])
+@app.route('/download', methods=['GET', 'POST'])
 def download():
-    path = request.form['Download']
-    return send_file(path, as_attachment=True)
-
-
-# route for error page
-@app.route('/error', methods=['GET', 'POST'])
-def error():
     cache.clear()
     if request.method == 'POST':
-        return render_template('error.html')
+        path = request.form['Download']
+        return send_file(path, as_attachment=True)
     else:
         return redirect('/')
 
 
+# route for query error page
+@app.route('/error', methods=['GET', 'POST'])
+def error():
+    cache.clear()
+    if request.method == 'POST':
+        return render_template('query_error.html')
+    else:
+        return redirect('/')
+
+
+# route for handling errors
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return render_template("error.html", code=str(e.code), description=str(e.description)), str(e.code)
+    # handle non-HTTP exceptions only
+    return render_template("error.html", code=500, description='generic error'), 500
+
+
 if __name__ == '__main__':
-    # app.run()
+    app.run()
     # DEBUG
-    app.run(debug=True)
+    # app.run(debug=True)
